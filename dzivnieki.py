3d4514b5042e552e8e0f205ac5ee43d3c9b91e10
Zane Udodova <zane.udodova@ventspils.edu.lv>
2025-01-16 09:35:17 +0200

    commit3

--- dzivnieki.py
+++ dzivnieki.py
@@ -11,10 +11,13 @@
 #git commit -m"komentars"
 #git push origin
 
+from abc import abstractmethod
+
 class Dzivnieks: 
     def __init__(self, vards, kajas): 
         self.vards = vards 
         self.kajas = kajas 
+    @abstractmethod
     def skanja(self): 
         print("random animal noise")
     def __str__(self):
@@ -25,23 +28,45 @@ class Dzivnieks:
 class Suns(Dzivnieks):
     def __init__(self, vards, kajas): 
         super().__init__(vards, kajas)
+        self.vards = "Sunītis "+self.vards
     def skanja(self):
         print("vau vau")
 
 class Kakis(Dzivnieks):
     def __init__(self, vards, kajas): 
         super().__init__(vards, kajas)
+        self.vards = "Minkāns "+self.vards
     def skanja(self):
         print("mrrrrrr")
 
-d1=Dzivnieks("Gauja",4)
+class Govs(Dzivnieks):
+    def skanja(self):
+        print("muuuuuuuuuuuuu")
+
+d1=Govs("Gauja",4)
 print(d1)
 d1.skanja()
 
-d2=Suns("Bruno",9)
+d2=Suns("Bruno",4)
 print(d2)
 d2.skanja()
 
-d3=Kakis("Fredis",12)
+d3=Kakis("Fredis",4)
 print(d3)
 d3.skanja()
+
+dzivniekuSaraksts = []
+dzivniekuSaraksts.append(Suns("Bruno", 4))
+dzivniekuSaraksts.append(Suns("Šeila", 4))
+dzivniekuSaraksts.append(Suns("Pepa", 4))
+dzivniekuSaraksts.append(Suns("Riko", 4))
+dzivniekuSaraksts.append(Kakis("Fredis", 4))
+dzivniekuSaraksts.append(Kakis("Rūdis", 4))
+dzivniekuSaraksts.append(Kakis("Muris", 4))
+dzivniekuSaraksts.append(Govs("Gauja", 4))
+dzivniekuSaraksts.append(Govs("Roja", 4))
+
+print("#################################")
+for dzivnieks in dzivniekuSaraksts:
+    print (dzivnieks)
+    dzivnieks.skanja()
