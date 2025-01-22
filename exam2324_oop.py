09f8f32893316a08e45b78411e6dbdc2bcf44ff6
Zane Udodova <zane.udodova@ventspils.edu.lv>
2025-01-22 15:43:34 +0200

    commit6

--- exam2324_oop.py
+++ exam2324_oop.py
@@ -33,13 +33,40 @@ class SakumskolasSkolotajs(Skolotajs):
     klase = "x.i"
     def __init__(self):
         self.skolotajaTips=1
+    def ievade(self):
+        self.uzvards = input("Ievadiet sākumskolas skolotāja uzvārdu: ")
+        self.klase = input("Ievadiet skolotāja klasi: ")
+        self.stunduSkNed = input("Ievadiet skolotāja stundu skaitu: ")
     def izvade(self):
-        print(f"Sākumskolas (tips - {self.skolotajaTips}) skolotājs {self.uzvards}")
+        print(f"Sākumskolas (tips - {self.skolotajaTips}) skolotājs {self.uzvards} māca {self.stunduSkNed} stundas {self.klase} klasē.")
 
 class VidusskolasSkolotajs(Skolotajs):
+    def __init__(self):
+        self.skolotajaTips=3
     pirmaisPrieksmets = "x priekšmets"
     otraisPrieksmets = "y priekšmets"
-    abuPrieksmetuKopSk = 0
+    pirmaisPrieksmetsStundas = 0
+    otraisPrieksmetsStundas = 0
+    stunduSkNedela = 0
+    def ievade(self):
+        self.uzvards = input("Ievadiet vidusskolas skolotāja uzvārdu: ")
+        self.pirmaisPrieksmets = input("Ievadiet pirmo pasniegto priekšmetu: ")
+        self.pirmaisPrieksmetsStundas = input("Ievadiet priekšmeta stundu skaitu: ")
+        self.otraisPrieksmets = input("Ievadiet otro pasniegto priekšmetu: ")
+        self.otraisPrieksmetsStundas = input("Ievadiet priekšmeta stundu skaitu: ")
+    def cikStundasKopa(self):
+        self.stunduSkNedela = self.pirmaisPrieksmetsStundas + self.otraisPrieksmetsStundas
+    def izvade(self):
+        print(f"Vidusskolas (tips - {self.skolotajaTips}) skolotājs {self.uzvards} māca šādus priekšmetus: {self.pirmaisPrieksmets} un {self.otraisPrieksmets} kopā {self.stunduSkNedela} stundas")
+
+
+
 
 ss1 = SakumskolasSkolotajs()
+ss1.ievade()
 ss1.izvade()
+
+vs1 = VidusskolasSkolotajs()
+vs1.ievade()
+vs1.izvade()
+
