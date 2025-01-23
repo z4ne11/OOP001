0d0973f16858146a1108558212e29c8542e85084
Zane Udodova <zane.udodova@ventspils.edu.lv>
2025-01-23 09:42:39 +0200

    commit7

---
+++ exam2223_oop.py
@@ -0,0 +1,19 @@
+#git add . 
+#git commit -m"komentars"
+#git push origin
+
+'''
+class csdd():
+    def __init__(self, zimols, modelis, datums, masa, degviela):
+        self.zimols = zimols
+        self.modelis = modelis
+        self.datums = datums
+        self.masa = masa
+        self.degviela = degviela
+    def __str__(self):
+        return f"zīmols: {self.zimols} \nmodelis: {self.modelis} \nreģistrācijas datums: {self.datums} \npilna masa: {self.masa} \ndegvielas veids: {self.degviela}" 
+
+masina=csdd("Audi","A4", "22.10.20219", 1800, "BG")
+print(masina)
+'''
+
